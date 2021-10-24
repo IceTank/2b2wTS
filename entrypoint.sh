921f79a188210586a4a8a04c13395accbdb899b4
IceTank <niklaslehle@gmail.com>
2021-10-24 15:43:43 +0200

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
-while ! $(ping -q -c 1 -W 1 8.8.8.8 >/dev/null)
+ping -q -c 1 -W 1 8.8.8.8 >/dev/null
+while [ $? != 0 ]
 do
   echo "IPv4 is down"
   sleep 2
+  ping -q -c 1 -W 1 8.8.8.8 >/dev/null
 done
 echo "Has internet connection"
 
